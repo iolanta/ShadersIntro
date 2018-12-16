4584bf7ad25b1830f52218b06fb18be1c7ee726f
Takatrov Evgeny <je.mouth@gmail.com>
2018-12-16 20:16:28 +0300

    wrappers

--- GLproj/GLShader.h
+++ GLproj/GLShader.h
@@ -21,11 +21,12 @@ public:
 class VariableData {
 public:
 	std::string name;
+	GLint location;
 	GLenum type;
 	GLsizei size;
 
 	VariableData();
-	VariableData(std::string n, GLenum t, GLsizei sz);
+	VariableData(GLint  l,std::string n, GLenum t, GLsizei sz);
 };
 
 class GLShader {
@@ -33,7 +34,16 @@ public:
 	GLShader();
 	~GLShader();
 
-	int load_shader(std::string filename, GLenum shader_type);
+	bool load_shader(std::string filename, GLenum shader_type);
+
+	bool setUniformfv4(std::string name, const GLfloat  * vec4);
+	bool setUniformfv3(std::string name,  const  GLfloat  * vec3);
+	bool setUniformfv2(std::string name,  const   GLfloat  *vec2);
+	bool setUniformmat4(std::string name, bool transpose,   const GLfloat * mat);
+	bool setUniform1i(std::string name,  const GLint value);
+
+	GLint getAttributeLocation(std::string name);
+
 
 private:
 	GLuint ShaderProgram;
@@ -41,6 +51,7 @@ private:
 
 	std::map<int, ShaderData> shaders;
 	std::map<std::string, VariableData> attributes;
+	std::map<std::string, VariableData> uniforms;
 
 	std::string loadSourceFile(const std::string& source_file_name);
 
@@ -51,5 +62,8 @@ private:
 	void linkProgram(int vertex_id, int fragment_id);
 
 	void checkOpenGLerror();
+
+
+
 };
 
